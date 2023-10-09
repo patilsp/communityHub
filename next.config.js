ab68d778900d57b1b2eafb15afc7f5bc373785a2
unknown <Dell@RN-BLR-LT-18.Fugo001.net>
2023-10-09 09:16:51 +0530

    new changes

--- next.config.js
+++ next.config.js
@@ -1,25 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ["mongoose"],
-  },
   images: {
     domains: ['uploadthing.com', 'lh3.googleusercontent.com'],
   },
-  // webpack(config) {
-  //   config.experiments = {
-  //     ...config.experiments,
-  //     topLevelAwait: true,
-  //   }
-  //   return config
-  // }
-  typescript: {
-    ignoreBuildErrors: true,
+  experimental: {
+    appDir: true
   },
-  eslint: {
-    ignoreDuringBuild: true,
-  },
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
+  // eslint: {
+  //   ignoreDuringBuild: true,
+  // },
 }
 
 module.exports = nextConfig
+
+
+
--- src/components/renderers/CustomImageRenderer.tsx
+++ src/components/renderers/CustomImageRenderer.tsx
@@ -7,7 +7,13 @@ function CustomImageRenderer({ data }: any) {
 
   return (
     <div className='relative w-full min-h-[15rem]'>
-      <Image alt='image' className='object-contain' fill src={src} />
+      <Image 
+       alt='image' 
+       className='object-contain rounded-sm' 
+       width={500}
+       height={300}
+       sizes="(max-width: 640px) 100vw, 640px"
+       src={src} />
     </div>
   )
 }
